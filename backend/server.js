f2e08b60f3edb5f3af6ef151e9d6d69ef92d0fc3
lealem <birhanumelesse12@gmail.com>
2024-12-04 23:50:33 +0300

    set up db connection and created user Schema

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 
 import express from "express";
-import "dotenv/config"
+import "dotenv/config";
+import connectDB from "./config/db.config.js";
 
 
 const app = express();
 
 
-
+connectDB();
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Server is running on port: ${process.env.PORT}`);
 });
